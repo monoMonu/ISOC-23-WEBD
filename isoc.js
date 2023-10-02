990e6aca0cd4cd3cd89b7a05c4997c2666eb9b1a
Monu <142221687+monoMonu@users.noreply.github.com>
2023-10-02 22:52:43 +0530

    Update isoc.js

--- isoc.js
+++ isoc.js
@@ -1,64 +1,54 @@
-/*let navbarMenu = document.querySelector('.navbar-menu');
-let dropdownIsOpen = false;
-
-// Handle dropdown menu toggle
-navbarMenu.addEventListener('click', (event) => {
-  if (event.target.classList.contains('dropdown-toggler')) {
-    let target = document.querySelector(`#${event.target.dataset.dropdown}`);
-
-    if (target) {
-      if (target.classList.contains('show')) {
-        target.classList.remove('show');
-        dropdownIsOpen = false;
-      } else {
-        target.classList.add('show');
-        dropdownIsOpen = true;
-      }
-    }
-  }
-});
-
-// Handle closing dropdowns if a user clicks outside
-document.body.addEventListener('click', (event) => {
-  if (dropdownIsOpen) {
-    navbarMenu.querySelectorAll('.dropdown').forEach((dropdown) => {
-      let targetIsDropdown = dropdown == event.target;
-      let clickedOnDropdownToggle = event.target.classList.contains('dropdown-toggler');
-
-      if (clickedOnDropdownToggle) {
-        return;
-      }
-
-      if (!targetIsDropdown && !dropdown.contains(event.target)) {
-        dropdown.classList.remove('show');
-      }
-    });
-  }
-});
-
-// Open links in mobiles
-function handleSmallScreens() {
-  document.querySelector('.navbar-toggler').addEventListener('click', () => {
-    if (!navbarMenu.classList.contains('active')) {
-      navbarMenu.classList.add('active');
-    } else {
-      navbarMenu.classList.remove('active');
-    }
-  });
-}
-
-handleSmallScreens();*/
-
 
 const hamburger = document.querySelector("#hamburger");
 const main_nav = document.querySelector(".main-nav");
 hamburger.addEventListener("click", ()=>{
-    if(!hamburger.classList.contains("cross")){
-        main_nav.classList.add("show-nav");
-        hamburger.classList.add("cross");
-    }
-    else {
-        main_nav.classList.remove("show-nav");
-        hamburger.classList.remove("cross");
-    }
+  if(!hamburger.classList.contains("cross")){
+      main_nav.classList.add("show-nav");
+      hamburger.classList.add("cross");
+  }
+  else {
+      main_nav.classList.remove("show-nav");
+      hamburger.classList.remove("cross");
+  }
 })
+
+const swiper = new Swiper('.swiper', {
+// Optional parameters
+direction: 'horizontal',
+loop: true,
+
+// If we need pagination
+/*pagination: {
+  el: '.swiper-pagination',
+  type: "bullets",
+},*/
+effect : "cube",
+cubeEffect : {
+  slideShows : false,
+  
+},
+autoplay:{
+  delay: 2000,
+}
+
+});
+
+
+// ShareBtn Event Handling
+const shareBtn = document.querySelectorAll(".shareBtn");
+shareBtn.forEach( (el)=> el.addEventListener("click", event => {
+   navigator.share({
+      title: 'WebShare API Demo',
+      url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+    .catch(console.error);
+}));
+
+// Nav Bar Options click event handling
+/*main_nav.children.forEach((el)=>{
+  el.addEventListener("click", ()=>{
+      
+  })
+})*/
